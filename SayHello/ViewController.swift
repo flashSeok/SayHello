082b270e4511a464a6b48717b0d1bdc222536926
Henry Kim <flashSeok@gmail.com>
2022-07-27 14:20:26 +0900

    "앱 올리기"

--- SayHello/ViewController.swift
+++ SayHello/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        mainLabel.text = "안녕하세요"
+        mainLabel.textColor = UIColor.white
+        
+        // mainLabel.backgroundColor = #colorLiteral( 치면 아래처럼 나옴.
+        mainLabel.backgroundColor =  #colorLiteral(red: 0.1764705882, green: 0.7058823529, blue: 0, alpha: 1)
+        
+    }
+    
 }
 
