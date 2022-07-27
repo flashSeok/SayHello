9ff6d2fbf6f4496cd7dad9c0c8a03a79faf95028
Henry Kim <flashSeok@gmail.com>
2022-07-27 14:14:24 +0900

    Initial Commit

---
+++ SayHello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SayHello
+//
+//  Created by 김현석 on 2022/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
